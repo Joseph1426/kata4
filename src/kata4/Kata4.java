3438b3afcee099c1f5a581a06c541a51a05dc778
Usuario <Usuario@Usuario-PC>
2017-11-03 12:06:19 +0000

    Kata 4, versión 2, calcular histograma y visualizar a partir de emails

---
+++ src/kata4/Kata4.java
@@ -0,0 +1,19 @@
+
+package kata4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Kata4 {
+
+    
+    public static void main(String[] args) throws IOException {
+        String filename = "C:\\Users\\Usuario\\Desktop\\emails.txt";
+        ArrayList<String> mailList=MailListReader.read(filename);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
+    }
+    
+}
